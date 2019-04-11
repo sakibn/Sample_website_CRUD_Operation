27426f0bdeb6540b045720cb30569cff2acacd4a
saqibn <saqibn@vcu.edu>
2019-04-11 11:03:42 -0400

    fixed problem with fool proof in registration.inc.php

--- includes/sign.inc.php
+++ includes/sign.inc.php
@@ -7,9 +7,10 @@ if (isset($_POST['signup-submit'])) {
     $r_pass = test_input($_POST['r-pwd']);
     $first = test_input($_POST['first']);
     $last = test_input($_POST['last']);
-//    if (!isset($username) || isset($username) == 'null' || !isset($password) || isset($password) == 'null' || isset($r_pass) == 'null' || !isset($r_pass) || !isset($first) || isset($first) == 'null' || !isset($last) || isset($last) == 'null') {
-//        header("location: ../registration.php?error=emptyfield1%first=" . $first . "&last" . $last);
-//        exit();
+    if ($username = ""  ||$password = ""|| $r_pass = "" || $first = "" || $last ="") {
+        header("location: ../registration.php?error=emptyfield1%first=" . $first . "&last" . $last);
+        exit();
+    }
 //    } else if (!preg_match("/^[a-zA-Z0-9*$/", $username)) {
 //        header("location: ../registration.php?error=invalidusername%first=" . $first . "&last" . $last);
 //        exit();
