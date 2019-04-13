ed7813835d8aec9e030722589e29ff47c63d2dc6
Mo Malik <32021980+BatmaniNRobin@users.noreply.github.com>
2019-04-13 18:26:08 -0400

    update inventory.php

--- inventory.php
+++ inventory.php
@@ -3,6 +3,28 @@
 <head>
 </head>
 <body>
-    
+<?php
+$db=mysqli_connect("localhost","carrental","carrental");
+
+if (mysqli_connect_errno())
+{
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$result = mysqli_query($db,"SELECT * FROM P_CARS");
+
+echo "<table border='1'>";
+while($row = mysqli_fetch_array($result))
+{
+    echo "<tr>";
+    echo "<td>" . $row['Model'] . "</td>";
+    echo "<td>" . $row['Shop Location'] . "</td>";
+    echo "<td>" . $row['Mileage'] . "</td>";
+    echo "</tr>";
+}
+echo "</table>";
+
+mysqli_close($db);
+?>
 </body>
 </html>
