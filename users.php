74d128d589b05c58731e14564e1d994511282025
Mo Malik <32021980+BatmaniNRobin@users.noreply.github.com>
2019-04-13 13:37:55 -0400

    update

--- users.php
+++ users.php
@@ -2,14 +2,16 @@
 <head>
 </head>
 <body>
-
-    <table>
-        <th>User Email</th>
-        <th>Account Type</th>
-        <th>Permissions</th>
-        <th>Actions</th>
-            <td><input type="button" value="edit permissions" onclick=editPermissions()/> </td>
-            <td><input type="button" value="delete user" onclick=deleteUser()/> </td>
+    <table style=>
+        <tr>
+            <th>User Email</th>
+            <th>Account Type</th>
+            <th>Permissions</th>
+            <th>Actions
+                <td><input type="button" value="edit permissions" onclick=editPermissions()/> </td>
+                <td><input type="button" value="delete user" onclick=deleteUser()/> </td>
+            </th>
+        </tr>
     </table>
     <?php   function editPermissions(){
             //
