f6804b8fc78803177ea5ebf8147d5f971bd9d982
Mo Malik <32021980+BatmaniNRobin@users.noreply.github.com>
2019-04-13 10:50:01 -0400

    update users.php

--- users.php
+++ users.php
@@ -11,6 +11,17 @@
             <td><input type="button" value="edit permissions" onclick=editPermissions()/> </td>
             <td><input type="button" value="delete user" onclick=deleteUser()/> </td>
     </table>
-    
+<?php   function editPermissions(){
+            //
+        } ?>
+<?php   function deleteUser(){
+            if(isset($_POST['btn_delete'])){
+                $username = mysql_real_escape_string($_POST['username_delete']);
+                $sql = mysql_query("DELETE FROM employees WHERE username = '$username'");
+                if($sql){
+                    echo "account deleted";
+                }
+            }
+        }
 </body>
 </html>
