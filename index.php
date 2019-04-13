20ba33610731bc72809a59dc1362fdcf7ea5b640
Mo Malik <32021980+BatmaniNRobin@users.noreply.github.com>
2019-04-13 17:31:05 -0400

    update

--- index.php
+++ index.php
@@ -21,15 +21,29 @@ require "header.php";
                 <li><a href="users.php">User access</a></li>
                 <li><a href="inventory.php">Available Inventory</a></li>
                 <li><a href="reservation.php">Reservation Form</a></li>
-            </ul>        
-        <?php endif; ?>
-        <?php if(!isset($_SESSION['username'])){
-                $db=mysqli_connect("localhost","carrental","carrental");
+            </ul> 
+        <?php else{
+            $db=mysqli_connect("localhost","carrental","carrental");
 
-                
-
-                mysqli_close($db);
+            if (mysqli_connect_errno()){
+                echo "Failed to connect to MySQL: " . mysqli_connect_error();
             }
+
+            $result = mysqli_query($db,"SELECT * FROM P_CAR");
+
+            echo "<table border='1'>";
+            while($row = mysqli_fetch_array($result)){
+                echo "<tr>";
+                echo "<td>" . $row['Model'] . "</td>";
+                echo "<td>" . $row['Shop Location'] . "</td>";
+                echo "<td>" . $row['Mileage'] . "</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+
+            mysqli_close($db);
+        }
+        <?php endif; ?>
         
     </div>
 </section>
