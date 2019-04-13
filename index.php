cc919a9e040d883007c0d7fd94012eb9c1c54f4d
Mo Malik <32021980+BatmaniNRobin@users.noreply.github.com>
2019-04-13 17:56:30 -0400

    back to normal

--- index.php
+++ index.php
@@ -12,28 +12,6 @@
 <?php
 require "header.php";
 ?>
-<?php
-            $db=mysqli_connect("localhost","carrental","carrental");
-
-            if (mysqli_connect_errno()){
-                echo "Failed to connect to MySQL: " . mysqli_connect_error();
-            }
-
-            $result = mysqli_query($db,"SELECT * FROM P_CAR");
-
-            echo "<table border='1'>";
-            while($row = mysqli_fetch_array($result)){
-                echo "<tr>";
-                echo "<td>" . $row['Model'] . "</td>";
-                echo "<td>" . $row['Shop Location'] . "</td>";
-                echo "<td>" . $row['Mileage'] . "</td>";
-                echo "</tr>";
-            }
-            echo "</table>";
-
-            mysqli_close($db);
-        }
-        ?>
 <section class="main-container">
     <div class="main-wrapper">
         <h2>Home</h2>
