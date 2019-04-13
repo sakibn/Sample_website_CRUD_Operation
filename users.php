37f1c8acd3abba7b46ac38ba0f90ac3d749e92be
Mo Malik <32021980+BatmaniNRobin@users.noreply.github.com>
2019-04-13 13:30:45 -0400

    update users.php

--- users.php
+++ users.php
@@ -1,9 +1,7 @@
 <html>
 <head>
 </head>
-<?php require "dbh.inc.php";?>
 <body>
-
     <table>
         <th>User Email</th>
         <th>Account Type</th>
@@ -12,7 +10,5 @@
             <td><input type="button" value="edit permissions" onclick=editPermissions()/> </td>
             <td><input type="button" value="delete user" onclick=deleteUser()/> </td>
     </table>
-
-<?php require "footer.php";?>
 </body>
 </html>
