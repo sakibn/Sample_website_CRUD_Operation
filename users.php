6f5c13bfc4f3c0a2da9a6a57f9cc58113fa741b1
Mo Malik <32021980+BatmaniNRobin@users.noreply.github.com>
2019-04-13 13:55:52 -0400

    update users.php

--- users.php
+++ users.php
@@ -8,6 +8,33 @@
 </style>
 </head>
 <body>
+<?php
+$con=mysqli_connect("example.com","peter","abc123","my_db");
+// Check connection
+if (mysqli_connect_errno())
+{
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$result = mysqli_query($con,"SELECT * FROM P_CUSTOMERS");
+
+echo "<table border='1'>
+<tr>
+<th>Firstname</th>
+<th>Lastname</th>
+</tr>";
+
+while($row = mysqli_fetch_array($result))
+{
+    echo "<tr>";
+    echo "<td>" . $row['FirstName'] . "</td>";
+    echo "<td>" . $row['LastName'] . "</td>";
+    echo "</tr>";
+}
+echo "</table>";
+
+mysqli_close($con);
+?>
     <table>
         <tr>
             <th>User Email</th>
