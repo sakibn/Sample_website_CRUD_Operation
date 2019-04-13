588a49364abad66dc946cf26c4296e00a22d3542
Mo Malik <32021980+BatmaniNRobin@users.noreply.github.com>
2019-04-13 17:06:49 -0400

    update index.php

--- index.php
+++ index.php
@@ -10,22 +10,14 @@ require "header.php";
     <div class="main-wrapper">
         <h2>Home</h2>
         <?php if(isset($_SESSION['username'])): ?>
-            <p>you are logged in or something like that</p>
             <ul>
                 <li><a href="database.php">Database/excel</a></li>
                 <li><a href="users.php">User access</a></li>
                 <li><a href="inventory.php">Available Inventory</a></li>
                 <li><a href="reservation.php">Reservation Form</a></li>
-            </ul>
-        <?php else: ?>
-            <p>you are logged out</p>
-        
+            </ul>        
         <?php endif; ?>
     </div>
 </section>
-
-<?php
-require "footer.php";
-?>
 </body>
 </html>
