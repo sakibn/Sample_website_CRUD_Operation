894d572c719cfe0323b8d623136033d404c80daa
Mo Malik <32021980+BatmaniNRobin@users.noreply.github.com>
2019-04-13 13:29:26 -0400

    update users.php

--- users.php
+++ users.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html>
 <html>
 <head>
 </head>
@@ -13,31 +12,7 @@
             <td><input type="button" value="edit permissions" onclick=editPermissions()/> </td>
             <td><input type="button" value="delete user" onclick=deleteUser()/> </td>
     </table>
-<?php   function editPermissions(){
-            //
-        } ?>
-<?php
-        function addUser(){
-            if(isset($_POST['btn_add'])){
-                $username = mysql_real_escape_string($_POST['username_add']);
-                $sql = mysql_query("INSERT VALUES INTO customers($username, $pwd, $role");
-                if($sql){
-                    echo "account added";
-                }
-            }
-        }
 
-?>
-<?php   function deleteUser(){
-            if(isset($_POST['btn_delete'])){
-                $username = mysql_real_escape_string($_POST['username_delete']);
-                $sql = mysql_query("DELETE FROM employees WHERE username = '$username'");
-                if($sql){
-                    echo "account deleted";
-                }
-            }
-        }
-?>
-<?php require "footer.php"?>
+<?php require "footer.php";?>
 </body>
 </html>
