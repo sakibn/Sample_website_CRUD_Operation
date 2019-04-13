619bea9e088b248581abe1b6b1ac7d4e411758c4
Mo Malik <32021980+BatmaniNRobin@users.noreply.github.com>
2019-04-13 11:38:51 -0400

    update

--- index.php
+++ index.php
@@ -10,18 +10,18 @@ require "header.php";
 <section class="main-container">
     <div class="main-wrapper">
         <h2>Home</h2>
-        <?php if(isset($_SESSION['username'])){
-            echo 'you are logged in';
-            echo '<ul>'
-            echo '<li><a href="database.php">Database/excel</a></li>'
-            echo '<li><a href="users.php">User access</a></li>'
-            echo '<li><a href="inventory.php">Available Inventory</a></li>'
-            echo '<li><a href="reservation.php">Reservation Form</a></li>'
-            echo '</ul>'
-        }else{
-            echo 'you are logged out';
-        }
-        ?>
+        <?php if(isset($_SESSION['username'])): ?>
+            <p>you are logged in</p>
+            <ul>
+                <li><a href="database.php">Database/excel</a></li>
+                <li><a href="users.php">User access</a></li>
+                <li><a href="inventory.php">Available Inventory</a></li>
+                <li><a href="reservation.php">Reservation Form</a></li>
+            </ul>
+        <?php else: ?>
+            <p>you are logged out</p>
+        
+        <?php endif; ?>
     </div>
 </section>
 
