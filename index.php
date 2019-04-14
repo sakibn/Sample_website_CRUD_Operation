e88410b296c4bd609601e2ca6bd1d911bb46479e
Mo Malik <32021980+BatmaniNRobin@users.noreply.github.com>
2019-04-14 01:51:48 -0400

    .css

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ require "header.php";
 ?>
 <section id="c3" class="main-container">
     <div class="main-wrapper">
-        <h2 style="color: blue">Home</h2>
+        <h2 style="color: powderblue">Home</h2>
         
         <?php if(isset($_SESSION['username'])): ?>
             <ul>
