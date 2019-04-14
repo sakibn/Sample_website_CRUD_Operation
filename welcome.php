795cd356562e5f05fc6522aff40f5fb2dc53ffe8
Mo Malik <32021980+BatmaniNRobin@users.noreply.github.com>
2019-04-14 02:48:38 -0400

    .css

--- welcome.php
+++ welcome.php
@@ -4,7 +4,7 @@
 session_start();
 
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("Location: ../index.php?login=fail");
+    header("Location: ../index.php");
     exit;
 }
 ?>
