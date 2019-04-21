680024cd9addaf666daab3ffa6c9dc6e9dee92ad
saqibn <saqibn@vcu.edu>
2019-04-21 17:17:33 -0400

    fixed test_header and test_footer

--- test/test_header.php
+++ test/test_header.php
@@ -8,7 +8,7 @@ session_start();
         Cartholemeu
     </title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/login_registration.css">
+    <link rel="stylesheet" href="login_registration.css">
 </head>
 <body>
 <header>
@@ -44,4 +44,3 @@ session_start();
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </header>
-</html>
