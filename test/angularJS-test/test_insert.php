6138b57a348fa526f328a9fe5672fd44d956bd5d
saqibn <saqibn@vcu.edu>
2019-04-22 20:21:49 -0400

    AHHHH, ITS ALIVE

--- test/angularJS-test/test_insert.php
+++ test/angularJS-test/test_insert.php
@@ -6,12 +6,14 @@ if ($conn->connect_error) {
 $message = '';
 
 $form_data = json_decode(file_get_contents("php://input"));
-
+$username = 'ASsdafDSAC';
+$password = hash('sha256','LKASDJF');
+$dob ='2012-02-01';
 $employee_first_name   = $form_data->first_name;
 $employee_last_name   = $form_data->last_name;
 
-$statement = $conn->prepare("INSERT INTO P_EMPLOYEES (employee_first_name, employee_last_name) VALUES (?,?)");
-$statement -> bind_param("ss", $employee_first_name, $employee_last_name);
+$statement = $conn->prepare("insert into P_EMPLOYEES (USERNAME, PWD, EMPLOYEE_FIRST_NAME, EMPLOYEE_LAST_NAME, EMPLOYEE_DOB) VALUE (?, ?, ?, ?, ?);");
+$statement -> bind_param("sssss", $username, $password, $employee_first_name, $employee_last_name, $dob);
 
 $statement->execute();
 
