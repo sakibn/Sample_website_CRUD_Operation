150b386adde70f1e816ec84db9ecbd1b2bd5c383
saqibn <saqibn@vcu.edu>
2019-04-24 17:02:05 -0400

    working on insert, edit password not working

--- test/angularJS-test/test_edit.php
+++ test/angularJS-test/test_edit.php
@@ -1,43 +1,56 @@
 <?php
 require 'include/dbh.test.php';
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 //include 'include/dbh.test.php';
 
 $message = '';
-$form_data = json_decode(file_get_contents("php://input"));
-$username = $form_data->USERNAME;
-$pwd = $form_data->PWD;
-$employee_first_name = $form_data->EMPLOYEE_FIRST_NAME;
-$employee_last_name = $form_data->EMPLOYEE_LAST_NAME;
-$employee_id = $form_data->EMPLOYEE_ID;
-
+//$form_data = json_decode(file_get_contents("php://input"));
+//$employee_id = $form_data->EMPLOYEE_ID;
+//$username = $form_data->USERNAME;
+//$pwd = $form_data->PWD;
+//$employee_first_name = $form_data->EMPLOYEE_FIRST_NAME;
+//$employee_last_name = $form_data->EMPLOYEE_LAST_NAME;
+$employee_id = '49';
+$username = 'yolo';
+$pwd = '49';
+$employee_last_name = 'yolo';
+$employee_last_name ='yolo';
+//echo 'working after the variable<br>';
 //$data = array(
 //    ':first_name' => $form_data->EMPLOYEE_FIRST_NAME,
 //    ':last_name' => $form_data->EMPLOYEE_LAST_NAME,
 //    ':id' => $form_data->EMPLOYEE_ID
 //);
-$statement = $conn ->prepare("SELECT PWD FROM P_EMPLOYEES WHERE EMPLOYEE_ID=?");
-$statement ->bind_param("i", $employee_id);
-$statement ->execute();
-$statement ->bind_result($result);
-$statement ->fetch();
-if($result == $pwd){
-    $statement = $conn->prepare("UPDATE P_EMPLOYEES SET employee_first_name = ?, employee_last_name = ?, USERNAME =? where EMPLOYEE_ID = ?;");
+$statement = $conn->prepare("SELECT PWD FROM P_EMPLOYEES WHERE EMPLOYEE_ID=?");
+$statement->bind_param("i", $employee_id);
+$statement->execute();
+$statement->bind_result($result);
+$statement->fetch();
+//echo 'working after the fetch';
+//echo $result;
+if ($result == $pwd) {
+//    echo 'working right after the if statement '.$result.'  '.$pwd;
+    $statement = $conn->prepare("UPDATE P_EMPLOYEES SET EMPLOYEE_FIRST_NAME = ?, EMPLOYEE_LAST_NAME = ?, USERNAME =? where EMPLOYEE_ID = ?;");
+    echo 'working after the prepare';
 //$query= ("UPDATE P_EMPLOYEES SET employee_first_name = :first_name, employee_last_name = :last_name where EMPLOYEE_ID = :id");
-    $statement -> bind_param("sssi", $employee_first_name, $employee_last_name,$username, $employee_id);
+    $statement->bind_param("sssi", $employee_first_name, $employee_last_name, $username, $employee_id);
+    echo 'working after the binding';
 //$statement= $connect->prepare($query);
-    if($statement -> execute())
-    {
+    if ($statement->execute()) {
         $message = 'Data Successfully Edited without password';
     }
-}else{
-    $pwd = hash('sha256',$pwd);
+    echo 'working after the pwd statement';
+} else {
+    echo 'working after the else statement';
+    $pwd = hash('sha256', $pwd);
     $statement = $conn->prepare("UPDATE P_EMPLOYEES SET employee_first_name = ?, employee_last_name = ?, USERNAME =?, PWD =? where EMPLOYEE_ID = ?;");
 //$query= ("UPDATE P_EMPLOYEES SET employee_first_name = :first_name, employee_last_name = :last_name where EMPLOYEE_ID = :id");
-    $statement -> bind_param("ssssi", $employee_first_name, $employee_last_name,$username, $pwd, $employee_id);
+    $statement->bind_param("ssssi", $employee_first_name, $employee_last_name, $username, $pwd, $employee_id);
 
 //$statement= $connect->prepare($query);
-    if($statement -> execute())
-    {
+    if ($statement->execute()) {
         $message = 'Data Successfully Edited';
     }
 }
@@ -45,7 +58,12 @@ if($result == $pwd){
 //$statement->bind_result($result);
 //$statement->fetch();
 $output = array(
-    'message' => $message
+    'message' => $message,
+    'username' => $username,
+    'password' => $pwd,
+    'first name' => $employee_first_name,
+    'last Name' => $employee_first_name,
+    'id' => $employee_id
 );
 
 echo json_encode($output);
