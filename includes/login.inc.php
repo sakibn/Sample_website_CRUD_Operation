9919e0ba09619f80850e5102b85e8429838db1ec
saqibn <saqibn@vcu.edu>
2019-04-25 21:48:15 -0400

    working on login.inc getting role from p_employees but cant

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -13,18 +13,39 @@ if (isset($_POST['login-submit'])) {
         header("Location: ../index.php?error=emptyfields");
         exit();
     } else {
-        $stmt = $conn->prepare("SELECT PWD FROM P_EMPLOYEES WHERE USERNAME=?;");
+        $stmt = $conn->prepare("SELECT PWD, ROLE FROM P_EMPLOYEES WHERE USERNAME=?;") or trigger_error($conn->error, E_USER_ERROR);
 //        print "userID = " . $uid;
-        $stmt->bind_param("s", $uid);
+        $stmt->bind_param("s", $uid) or trigger_error($stmt, E_USER_ERROR);
 //        print "working after the bind";
-        $stmt->execute();
+        $stmt->execute() or trigger_error($stmt->error, E_USER_ERROR);
 //        print "working after the execute";
 //        echo "\nworking after bind and execute";
-        $stmt->bind_result($result);
-        $stmt->fetch();
+//        $stmt->bind_result($result);
+//        $stmt->fetch();
+        ($result = $stmt->get_result()) or trigger_error($stmt->error, E_USER_ERROR);
+        $data[] = "";
+//        echo  '<br>working before the fetch';
+        if ($result->num_rows > 0) {
+            // output data of each row
+            echo  'working after the condition<br>';
+            var_dump($result);
+            while ($row = $result->fetch_assoc()) {
+//        echo "UserName: " . $row["USERNAME"]. " - Role " . $row["ROLE"]. "<br>";
+                $data[] = $row;
+            }
+//            echo '<br>getting data<br>';
+            json_encode($row);
+            echo $row;
+            exit();
+        }else{
+            echo 'not working';
+            exit();
+        }
+        echo '<br>working after the condition';
+            echo $data[0];
 //        echo "\nresult= " . $result;
 //        echo "<br> hash:".$hash;
-        if ($hash == $result) {
+        if ($hash == $data[0]) {
 //            echo "verified";
             session_start();
             $_SESSION['username'] = $uid;
